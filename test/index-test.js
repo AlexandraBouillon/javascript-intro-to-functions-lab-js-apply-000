04bb226ee71c5374d9c4d51884de73601e433cd6
Alexandra M Bouillon <alexandra.bouillon@gmail.com>
2020-09-15 18:43:47 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,6 @@
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    return string.toUpperCase();
     expect(shout('hello')).toEqual('HELLO')
   })
 })
